4aaa434b91ff2bc0df6d4eae1f0187b1e0afa564
Mr. Bishnu Prasad Khanal <bkhanal1@my.centennialcollege.ca>
2019-09-23 19:05:22 -0400

    Inital architecture created

--- OnlineBookServices/App_Start/BundleConfig.cs
+++ OnlineBookServices/App_Start/BundleConfig.cs
@@ -19,8 +19,10 @@ namespace OnlineBookServices
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bookServicesJs").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js",
+                      "~/Scripts/AdminMenu.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
