d54a52e07cabfe4643b0483fd404a30f0a2083f8
Mr. Bishnu Prasad Khanal <bkhanal1@my.centennialcollege.ca>
2019-09-23 19:43:48 -0400

    Book & Genre class added

---
+++ OnlineBookServices/Models/Book.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OnlineBookServices.Models
+{
+    public class Book
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string ISBN { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        [DataType(DataType.ImageUrl)]
+        public string ImageUrl { get; set; }
+        [Required]
+        [Range(0, 1000)] // this range gives availablity property, this stops going beyond 0
+        public int Availablity { get; set; }
+        [Required]
+        [DataType(DataType.Currency)] // gives the currency data type
+        public double Price { get; set; }
+        
+        [DisplayFormat(DataFormatString ="{0:MMM DD YYYY}")]
+        public DateTime? DateAdded { get; set; }
+        
+        [Required]
+        public int GenreId { get; set; }
+        public Genre Genre { get; set; } //this will invoke to the Genre class
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MMM DD YYYY}")]
+
+        public DateTime PublicationDate { get; set; }
+
+        [Required]
+        public int Pages { get; set; }
+        [Required]
+        public string ProductDimensions { get; set; }
+    }   
+
+}
