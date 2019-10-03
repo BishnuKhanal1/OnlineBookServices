7b5a5f6b338b5446a4daaff8962e900aeab0ffb9
Mr. Bishnu Prasad Khanal <bkhanal1@my.centennialcollege.ca>
2019-10-02 20:02:12 -0400

    Genre Controller and Genre View created

---
+++ OnlineBookServices/Controllers/GenreController.cs
@@ -0,0 +1,32 @@
+﻿using OnlineBookServices.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineBookServices.Controllers
+{
+    public class GenreController : Controller
+    {
+        
+        private ApplicationDbContext db; //added as using statement to initialize db
+
+        public GenreController() //constructor
+        {
+            db = new ApplicationDbContext(); 
+        }
+
+        // GET: Genre
+        public ActionResult Index() //here to display table with all rows in db
+                                    //and display complete genre
+        {
+            return View(db.Genre.ToList()); //retrives genre from db and converts to the list and passes to the view
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+        }
+    }
+}
